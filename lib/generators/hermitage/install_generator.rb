6419bfbce330186e8e2e872d6498e0ddd1443ad9
Alexander Borovykh <immaculate.pine@gmail.com>
2013-09-03 17:28:22 +0400

    Generator adds require statement to application.js file.

--- lib/generators/hermitage/install_generator.rb
+++ lib/generators/hermitage/install_generator.rb
@@ -7,6 +7,11 @@ module Hermitage
       def copy_initializer
         template 'hermitage.rb', 'config/initializers/hermitage.rb'
       end
+
+      def insert_require_into_application_js
+        inject_into_file 'app/assets/javascripts/application.js', "\n//= require hermitage", after: %r{^//= require +['"]?jquery['"]?$}
+      end
+      
     end
   end
 end
--- spec/generators/install_spec.rb
+++ spec/generators/install_spec.rb
@@ -2,7 +2,18 @@ require 'spec_helper'
 require 'genspec'
 
 describe 'hermitage:install' do
+  
+  within_source_root do
+    FileUtils.mkdir_p 'app/assets/javascripts'
+    FileUtils.touch 'app/assets/javascripts/application.js'
+  end
+
   it 'generates configuration file at config/initializer' do
     subject.should generate('config/initializers/hermitage.rb')
   end
+
+  it "should inject a require into application.js file" do
+    subject.should inject_into_file('app/assets/javascripts/application.js', "\n//= require hermitage", after: %r{^//= require +['"]?jquery['"]?$})
+  end
 end
+  
