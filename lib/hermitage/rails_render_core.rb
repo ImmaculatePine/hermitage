6fe8cd21f63dc8b80006a6c6a6a4514096f62985
Alexander Borovykh <immaculate.pine@gmail.com>
2013-09-11 20:27:38 +0400

    Rendering logic for the Rails apps incapsulated in RailsRenderCore class.

---
+++ lib/hermitage/rails_render_core.rb
@@ -0,0 +1,57 @@
+module Hermitage
+  
+  # This class performs all the rendering logic for Rails apps
+  class RailsRenderCore < ActionView::Base
+
+    def initialize(objects, options = {})
+      @objects = objects
+      @options = Configurator.options_for(objects, options)
+    end
+
+    # Renders gallery markup
+    def render
+      # Initialize the resulting tag
+      tag = ''
+
+      # Slice objects into separate lists
+      lists = slice_objects
+
+      # Render each list into `tag` variable
+      lists.each do |list|
+        items = list.collect { |item| render_link_for(item) }
+        tag << render_content_tag_for(items)
+      end
+
+      tag.html_safe
+    end
+
+    private
+
+    # Slices objects into separate lists if it's necessary
+    def slice_objects
+      unless @options[:each_slice]
+        [@objects]
+      else
+        @objects.each_slice(@options[:each_slice]).to_a
+      end
+    end
+
+    # Renders link to the specific image in a gallery
+    def render_link_for(item)
+      original_path = eval("item.#{@options[:original]}")
+      thumbnail_path = eval("item.#{@options[:thumbnail]}")
+      title = @options[:title] ? eval("item.#{@options[:title]}") : nil
+      image = image_tag(thumbnail_path, class: @options[:image_class])
+      link_to(image, original_path, rel: 'hermitage', class: @options[:link_class], title: title)
+    end
+
+    # Renders items into content tag
+    def render_content_tag_for(items)
+      content_tag(@options[:list_tag], class: @options[:list_class]) do
+        items.collect { |item| concat(content_tag(@options[:item_tag], item, class: @options[:item_class])) }
+      end
+    end
+
+  end
+
+end
--- lib/hermitage/view_helpers.rb
+++ lib/hermitage/view_helpers.rb
@@ -1,3 +1,5 @@
+require 'hermitage/rails_render_core' if defined? Rails
+
 module Hermitage
   
   module ViewHelpers
@@ -14,39 +16,10 @@ module Hermitage
     #   render_gallery_for album.photos # album.photos is array of Photo instances
     #
     # it will render the objects contained in array and will use :images (or :photos) as config name.
-    # Config names are formed by the class name of the first element in array by Configurator.
+    # Config names are formed by the class name of the first element in array.
     #
     def render_gallery_for(objects, options = {})
-      options = Configurator.options_for(objects, options)
-
-      # Create array of all list tags
-      lists = unless options[:each_slice]
-        [objects]
-      else
-        objects.each_slice(options[:each_slice]).to_a
-      end
-
-      # The resulting tag
-      tag = ''
-
-      # Render each list into `tag` variable
-      lists.each do |list|
-        # Array of items in current list
-        items = list.collect do |item|
-          original_path = eval("item.#{options[:original]}")
-          thumbnail_path = eval("item.#{options[:thumbnail]}")
-          title = options[:title] ? eval("item.#{options[:title]}") : nil
-          image = image_tag(thumbnail_path, class: options[:image_class])
-          link_to(image, original_path, rel: 'hermitage', class: options[:link_class], title: title)
-        end
-        
-        # Convert these items into content tag string
-        tag << content_tag(options[:list_tag], class: options[:list_class]) do
-          items.collect { |item| concat(content_tag(options[:item_tag], item, class: options[:item_class])) }
-        end
-      end
-
-      tag.html_safe
+      RailsRenderCore.new(objects, options).render if defined? Rails
     end
 
   end
